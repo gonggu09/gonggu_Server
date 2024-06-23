f7633e24af390b0073ae41165ec19003cd8b9e34
Yeonjae Choi <fox2yj@naver.com>
2024-06-23 14:01:59 +0900

    [chore] static, media 경로 설정

--- config/settings.py
+++ config/settings.py
@@ -114,3 +114,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#media 루트 설정
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+#static 루트 설정
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
