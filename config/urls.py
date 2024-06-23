243faa9c79de2a6dc3c723ff73a6ea26af80211d
Yeonjae Choi <fox2yj@naver.com>
2024-06-23 17:38:00 +0900

    설정 변경

--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('gonggu/', include('gonggu.urls')),
 
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA__ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?:.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
